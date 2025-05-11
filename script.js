20e5e9dc3ccfcc70035a6612c194ab5737d1635a
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-11 13:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const byteSize = (str) => {
   // write your code here
+	let arr=str.split('');
+	
 	if(str.length === 0){
 		return 0;
 	}
-	return str.length;
+	return arr.length;
+	
 };
 
 // Do not change the code below
+
 const str = prompt("Enter some string.");
 alert(byteSize(str));
